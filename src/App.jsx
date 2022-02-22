c62682e465fcf210d9992a0fb7c6969dd2505ced
juanfvalencia <90592176+juanfvalencia@users.noreply.github.com>
2022-02-21 22:30:03 -0500

    Se añaden cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import Header from './components/header';
-import Navbar from './components/navbar';
-import ProductSection from './components/productSection';
 import Index from './pages';
 
 import './styles/style.css';

194b2dcaf56051c5e86ddd2d46022a102d771579
juanfvalencia <90592176+juanfvalencia@users.noreply.github.com>
2022-02-21 22:28:41 -0500

    Se integra pagina index

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import Header from './components/header';
 import Navbar from './components/navbar';
 import ProductSection from './components/productSection';
+import Index from './pages';
 
 import './styles/style.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Navbar/>
-      <main>
-        <section></section>
-        <ProductSection/>
-        <section></section>
-      </main>
-      <footer></footer>
+      <Index/>
     </div>
   );
 }
---
+++ src/pages/index.jsx
@@ -0,0 +1,21 @@
+import Header from "../components/header";
+import Navbar from "../components/navbar";
+import ProductSection from "../components/productSection";
+
+const Index = () =>{
+
+    return(
+        <div>
+            <Header/>
+            <Navbar/>
+            <main>
+                <section></section>
+                <ProductSection/>
+                <section></section>
+                </main>
+            <footer></footer>
+        </div>
+    )
+}
+
+export default Index;
